5dde49d3cf4b752f4a2200cd85bf8e1505e629fa
jdmedlock <jdmedlock@gmail.com>
2023-04-06 09:49:27 -0500

    Feature: Update positioning of Certificate elements

--- index.js
+++ index.js
@@ -2,31 +2,37 @@ import { PDFDocument, StandardFonts, rgb } from "pdf-lib"
 import { writeFileSync, readFileSync } from "fs"
 
 async function createPDF() {
-  const document = await PDFDocument.load(readFileSync("./certtemplate.pdf"));
+  const document = await PDFDocument.load(readFileSync("./assets/Chingu Voyage Completion Certificate - Template.pdf"))
 
-  const courierBoldFont = await document.embedFont(StandardFonts.Courier);
-  const firstPage = document.getPage(0);
+  const helveticaFont = await document.embedFont(StandardFonts.Helvetica)
+  const helveticaBoldObliqueFont = await document.embedFont(StandardFonts.HelveticaBoldOblique)
+  const firstPage = document.getPage(0)
 
-  firstPage.moveTo(72, 570);
-  firstPage.drawText(new Date().toUTCString(), {
-    font: courierBoldFont,
-    size: 12,
-  });
+  const dateFormat = {
+    month: "long",
+    day: "2-digit",
+    year: "numeric",
+  }
+  const issueDate = 'on '.concat(Intl.DateTimeFormat('en',dateFormat).format(new Date()))
+  firstPage.moveTo(700, 450)
+  firstPage.drawText(issueDate, {
+    font: helveticaFont,
+    size: 28,
+  })
 
-  firstPage.moveTo(105, 530);
-  firstPage.drawText("Ms. Jane,", {
-    font: courierBoldFont,
-    size: 12,
-  });
+  firstPage.moveTo(700, 600);
+  firstPage.drawText("Jane Doe", {
+    font: helveticaBoldObliqueFont,
+    size: 36,
+  })
 
-  firstPage.moveTo(72, 330);
-  firstPage.drawText("John Doe \nSr. Vice President Engineering \nLogRocket", {
-    font: courierBoldFont,
-    size: 12,
-    lineHeight: 10,
-  });
+  firstPage.moveTo(675, 370);
+  firstPage.drawText("Voyage #44", {
+    font: helveticaBoldObliqueFont,
+    size: 36,
+  })
 
-  writeFileSync("jane-doe.pdf", await document.save());
+  writeFileSync("Completed Cert.pdf", await document.save())
 }
 
-createPDF().catch((err) => console.log(err));
+createPDF().catch((err) => console.log(err))
