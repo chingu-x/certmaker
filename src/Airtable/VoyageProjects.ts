44994c85ea5894dc6b5e24434920e881fd1af76f
jdmedlock <jdmedlock@gmail.com>
2026-08-11 16:36:19 -0500

    Fix: Correct reference to teams config parm

--- src/Airtable/VoyageProjects.ts
+++ src/Airtable/VoyageProjects.ts
@@ -40,7 +40,8 @@ const getSuccessfulVoyagers = async (voyageName: string, roles: string, teams: s
     } else {
       // Create a filter to extract team members from specific teams who 
       // successfully completed
-      const teamNumbers = (process.env.TEAMS ?? '').split(',')
+      const teamNumbers = (teams ?? '').split(',')
+      console.log(`getSuccessfulVoyager - teamNumbers: ${ teamNumbers }`)
       const teamConditions = teamNumbers.map(teamNumber => {
         return '{'.concat('What is your Team number?}',' = ',teamNumber)
       })
